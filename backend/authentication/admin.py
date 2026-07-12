9d9ba0b3ac4d758a806db7ba36959dbfcf6077e0
SahusBhandary <sahusbhandary04@gmail.com>
2026-07-12 12:41:20 -0400

    Added Users to admin view

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ["email"]
+    list_display = ["email", "username", "is_staff", "is_active"]
+    fieldsets = (
+        (None, {"fields": ("email", "username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "username", "password1", "password2"),
+        }),
+    )
+    search_fields = ["email", "username"]
