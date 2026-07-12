d6a0f52cc297e93cf08f4ed56d6104e62a6c149a
SahusBhandary <sahusbhandary04@gmail.com>
2026-07-12 12:54:22 -0400

    Removed all-auth/google backend for now

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,16 +1,3 @@
-# from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
-# from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-# from dj_rest_auth.registration.views import SocialLoginView
-# from django.conf import settings
-# from django.utils.decorators import method_decorator
-# from django.views.decorators.csrf import csrf_exempt
-
-# @method_decorator(csrf_exempt, name='dispatch')
-# class GoogleLogin(SocialLoginView):
-#     adapter_class = GoogleOAuth2Adapter
-#     client_class = OAuth2Client
-#     callback_url = settings.GOOGLE_OAUTH_CALLBACK_URL
-
 from rest_framework import generics, permissions
 from django.contrib.auth import login
 
